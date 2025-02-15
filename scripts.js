6a690ca17fec7f890fc1cd9df80d29c4ca762207
Endregion <adamdeshadu@gmail.com>
2025-02-15 22:00:56 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,21 +1,26 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const links = document.querySelectorAll('nav a, .url-link');
+    const links = document.querySelectorAll('.url-link');
     const contents = document.querySelectorAll('.content');
     const toast = document.getElementById('toast');
+    const navLinks = document.querySelectorAll('nav a');
+
+    navLinks.forEach(navLink => {
+        navLink.addEventListener('click', function(event) {
+            event.preventDefault();
+            const targetId = this.dataset.target;
+            contents.forEach(content => {
+                content.classList.remove('active');
+            });
+            document.getElementById(targetId).classList.add('active');
+        });
+    });
 
     links.forEach(link => {
         link.addEventListener('click', function(event) {
-            const targetId = this.dataset.target;
-            if (targetId) {
-                event.preventDefault();
-                contents.forEach(content => {
-                    content.classList.remove('active');
-                });
-                document.getElementById(targetId).classList.add('active');
-            }
-            if (this.classList.contains('url-link')) {
-                showToast();
-            }
+            showToast();
+            setTimeout(() => {
+                window.open(this.href, '_blank');
+            }, 1000); // Adjust the timeout if needed
         });
     });
 
