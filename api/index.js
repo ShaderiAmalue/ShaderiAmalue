c811d2d067ad003d273beeab67117036b2175f70
Endregion <adamdeshadu@gmail.com>
2025-02-15 18:13:26 +0300

    Delete api directory

--- api/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const postsRouter = require('./posts');
-const app = express();
-
-app.use(express.json());
-app.use('/api/posts', postsRouter);
-
-module.exports = app;
